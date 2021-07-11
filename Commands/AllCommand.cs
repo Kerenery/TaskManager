f4a183f4a95b6d5eafa12d1436b0a0454f0360c9
Nick <djhitekdieant@gmail.com>
2021-07-11 15:45:16 +0300

    Union to display

--- Commands/AllCommand.cs
+++ Commands/AllCommand.cs
@@ -1,14 +1,13 @@
-﻿using System;
+﻿
 using Spectre.Console.Cli;
 using Spectre.Console;
-using System.Threading;
-using System.Collections.Generic;
-using System.Linq;
+
 
 namespace TaskManager.Commands
 {
     public class AllCommand : Command
     {
+        
         public override int Execute(CommandContext context)
         {
             TaskRegistry t = new();
@@ -22,67 +21,12 @@ namespace TaskManager.Commands
 
             else
             {
-                AnsiConsole.WriteLine("1");
-                var table = new Table().Centered();
-
-
-                table.AddColumn("Id");
-                table.AddColumn("Task");
-                table.AddColumn("Deadline");
-                table.AddColumn("Completed");
-                table.AddColumn("SubTasks").Centered();
-                table.Title("[[ [mediumpurple2]To do task list, hurry up![/] ]]");
-
-                foreach (KeyValuePair<int, Task> kvp in t.ListAllTasks())
-                {
-                    if (!t.ParentExists(kvp.Key))
-                    {
-
-                        var subtable = new Table()
-                            .Border(TableBorder.Rounded)
-                            .BorderColor(Color.Green)
-                            .AddColumn(new TableColumn("[u]Id[/]"))
-                            .AddColumn(new TableColumn("[u]Task[/]"))
-                            .AddColumn(new TableColumn("[u]Completed[/]"));
-                        subtable.BorderColor(Color.DarkOliveGreen3_2);
-
-                        foreach (var sub in kvp.Value.Child)
-                        {
-                            subtable.AddRow(new Markup($"[underline yellow]{t.GetId(sub.Name)}[/]"),
-                                new Markup($"{(t.IsTaskDone(t.GetId(sub.Name)) ? $"[underline green]{sub.Name}[/]" : $"[gold1]{sub.Name}[/]")}"),
-                                new Markup($"{(t.IsTaskDone(t.GetId(sub.Name)) ? "[green]Yes![/]" : "[red]No![/]") }"));
-                        }
-
-                        table.AddRow(new Markup($"[underline yellow]{kvp.Key}[/]"),
-                             new Markup($"{(t.IsTaskDone(kvp.Key) ? $"[underline green]{kvp.Value.Name}[/]" : $"[gold1]{kvp.Value.Name}[/]")}"),
-                             new Markup($"[aqua]{t.GetTaskDeadline(kvp.Key)}[/]"),
-                             new Markup($"{(t.IsTaskDone(kvp.Key) ? "[green]Yes![/]" : "[red]No![/]") }"),
-                             subtable);
-                    }
-                };
-
-                AnsiConsole.Render(table);
-
-                var tree = new Tree("[gold1]Groups[/]")
-                 .Style(Style.Parse("aqua"))
-                 .Guide(TreeGuide.BoldLine);
-
-                foreach (var kvp in t.ListAllGroups())
-                {
-                    var groupnode = tree.AddNode($"[green]{kvp.Value.Name}[/]");
-                    foreach (var child in t.ListAllChildren(kvp.Value))
-                    {
-                        groupnode.AddNode($"[green]id[/]: [underline yellow]{t.GetId(child.Name)}[/] [green]Task[/]: [gold1]{child.Name}[/]" +
-                            $" [green]Deadline:[/] [gold1]{t.GetTaskDeadline(t.GetId(child.Name))}[/] " +
-                            $"[green]Completed[/] {(t.IsTaskDone(t.GetId(child.Name)) ? "[green]Yes![/]" : "[red]No[/]!") }");
-                    }
-
-                }
-                AnsiConsole.Render(tree);
+                Display.DisplayTable(t);
+                // groups
+                Display.DisplayTree(t);
             }
 
             return 0;
-
         }
     }
 }
--- Commands/CompletedCommand.cs
+++ Commands/CompletedCommand.cs
@@ -11,28 +11,7 @@ namespace TaskManager.Commands
             taskRegistry.Load(@"D:\Downloads\book1.json");
             if (taskRegistry.ListAllTasks().Count != 0)
             {
-                var table = new Table().Centered();
-                
-                        table.BorderColor(Color.DarkOliveGreen3_2);
-                        table.SimpleHeavyBorder();
-                        table.AddColumn("Id");
-                        table.AddColumn("Task");
-                        table.AddColumn("Deadline");
-                        table.AddColumn("Completed");
-                        table.Title("[[ [mediumpurple2]Completed tasks btw![/] ]]");
-
-                        foreach (var kvp in taskRegistry.ListAllTasks())
-                        {
-                            if (kvp.Value is Task)
-                            {
-                                if (taskRegistry.IsTaskDone(kvp.Key))
-                                {
-                                    table.AddRow($"{kvp.Key}", $"{(taskRegistry.IsTaskDone(kvp.Key) ? $"[underline green]{kvp.Value.Name}[/]" : $"[gold1]{kvp.Value.Name}[/]")}", 
-                                        $"{taskRegistry.GetTaskDeadline(kvp.Key)}", $"{(taskRegistry.IsTaskDone(kvp.Key) ? "[green]Yes![/]" : "[red]No[/]!") }");
-                                }
-                            }
-
-                    };
+                Display.DisplayComleted(taskRegistry);
             }
             else
             {
