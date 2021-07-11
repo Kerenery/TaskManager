fff6a072ebf56ef8bce18b4359737055363d543e
Nick <djhitekdieant@gmail.com>
2021-07-11 15:45:16 +0300

    Union to display

--- Commands/TodayCommand.cs
+++ Commands/TodayCommand.cs
@@ -16,28 +16,7 @@ namespace TaskManager.Commands
             taskRegistry.Load(@"D:\Downloads\book1.json");
             if (taskRegistry.ListAllTasks().Count != 0)
             {
-                var table = new Table().Centered();
-                
-
-                        table.BorderColor(Color.DarkOliveGreen3_2);
-                        table.SimpleHeavyBorder();
-                        table.AddColumn("Id");
-                        table.AddColumn("Task");
-                        table.AddColumn("Deadline");
-                        table.AddColumn("Completed");
-                        table.Title("[[ [mediumpurple2]Today tasks![/] ]]");
-
-                        foreach (var kvp in taskRegistry.ListAllTasks())
-                        {
-                            if (kvp.Value is Task)
-                            {
-                                if (taskRegistry.TodayTasks(kvp.Key))
-                                {
-                                   table.AddRow($"{kvp.Key}", $"{(taskRegistry.IsTaskDone(kvp.Key) ? $"[underline green]{kvp.Value.Name}[/]" : $"[gold1]{kvp.Value.Name}[/]")}", 
-                                       $"{taskRegistry.GetTaskDeadline(kvp.Key)}", $"{(taskRegistry.IsTaskDone(kvp.Key) ? "[green]Yes![/]" : "[red]No[/]!") }");
-                                }
-                            }
-                    };
+                Display.DisplayToday(taskRegistry);
             }
             else
             {
